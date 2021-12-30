ff1509d8ec4c88881ced599747a4380f9ad4a296
jmeirink <jesse@meirink.com>
2021-12-30 12:15:34 -0600

    Updated app.js and added ejs file

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
 
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.sendFile(__dirname + "/weekend.html");
-  } else {
-    res.sendFile(__dirname + "/weekday.html");
+  if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else if (currentDay === 6) {
+    day = "Saturday";
+  } else if (currentDay === 0) {
+    day = "Sunday";
   }
+
+  res.render("list", {kindOfDay: day})
 });
 
 app.listen(3000, function(){
