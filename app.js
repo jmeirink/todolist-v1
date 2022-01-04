0a692512b6e9142907a866d393a43dfbb17977f6
jmeirink <jesse@meirink.com>
2022-01-03 18:23:20 -0600

    Updated ejs content

--- app.js
+++ app.js
@@ -7,31 +7,44 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
 
-  if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay === 5) {
-    day = "Friday";
-  } else if (currentDay === 6) {
-    day = "Saturday";
-  } else if (currentDay === 0) {
-    day = "Sunday";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+    console.log("Error: current day is equal to: " + currentDay);
   }
 
-  res.render("list", {kindOfDay: day})
+  res.render("list", {
+    kindOfDay: day
+  })
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
